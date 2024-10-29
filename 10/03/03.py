e931cf79f641399843775130d249e48579338d2d
pych1971 <pych1971@gmail.com>
2024-10-29 14:15:43 +0500

    2024-10-29-14-15

---
+++ 10/03/03.py
@@ -0,0 +1,5 @@
+text = 'footballcyberpunkextraterritorialityconversationalistblockophthalmoscopicinterdependencemamauserfff'
+
+result = {}
+for symbol in text:
+    result[symbol] = result.get(symbol, 0) + 1
