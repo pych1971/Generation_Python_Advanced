e9f870200cc2afdc038022d4250f39da7ee99c1d
pych1971 <pych1971@gmail.com>
2024-10-30 14:17:33 +0500

    2024-10-30-14-17

--- 10/03/04.py
+++ 10/03/04.py
@@ -1,2 +1,12 @@
 s = 'orange strawberry barley gooseberry apple apricot barley currant orange melon pomegranate banana banana orange barley apricot plum grapefruit banana quince strawberry barley grapefruit banana grapes melon strawberry apricot currant currant gooseberry raspberry apricot currant orange lime quince grapefruit barley banana melon pomegranate barley banana orange barley apricot plum banana quince lime grapefruit strawberry gooseberry apple barley apricot currant orange melon pomegranate banana banana orange apricot barley plum banana grapefruit banana quince currant orange melon pomegranate barley plum banana quince barley lime grapefruit pomegranate barley'
-dict_s = {}
+result = {}
+max_fruit = 0
+for fruit in s.split():
+    result[fruit] = result.get(fruit, 0) + 1
+    if result[fruit] > max_fruit:
+        max_fruit = result[fruit]
+result_fruit = 'zzz'
+for key, value in result.items():
+    if value == max_fruit and key < result_fruit:
+        result_fruit = key
+print(result_fruit)
