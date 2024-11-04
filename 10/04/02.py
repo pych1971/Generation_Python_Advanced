92d8d415f2c88cd4b154292dfabbca370f912ca8
pych1971 <pych1971@gmail.com>
2024-11-04 14:22:50 +0500

    2024-11-04-14-22

--- 10/04/02.py
+++ 10/04/02.py
@@ -1,11 +1,9 @@
 word1 = {}
-for i in input().lower():
-    if i not in '.,!?:;- ':
-        word1[i] = word1.get(i, 0) + 1
+for i in input():
+    word1[i] = word1.get(i, 0) + 1
 word2 = {}
-for i in input().lower():
-    if i not in '.,!?:;- ':
-        word2[i] = word2.get(i, 0) + 1
+for i in input():
+    word2[i] = word2.get(i, 0) + 1
 if word1 == word2:
     print('YES')
 else:
