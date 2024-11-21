5735c39bc232b2032ebc8f82130048e98e195b71
pych1971 <pych1971@gmail.com>
2024-11-21 11:14:29 +0500

    2024-11-21-11-14

---
+++ 11/01/02.py
@@ -0,0 +1,12 @@
+emails = {'nosu.edu': ['timyr', 'joseph', 'svetlana.gaeva', 'larisa.mamuk'],
+          'gmail.com': ['ruslan.chaika', 'rustam.mini', 'stepik-best'],
+          'msu.edu': ['apple.fruit', 'beegeek', 'beegeek.school'],
+          'yandex.ru': ['surface', 'google'],
+          'hse.edu': ['tomas-henders', 'cream.soda', 'zivert'],
+          'mail.ru': ['angel.down', 'joanne', 'the.fame.moster']}
+emails_list = []
+for i in emails:
+    for j in emails[i]:
+        emails_list.append(f'{j}@{i}')
+for i in sorted(emails_list):
+    print(i)
